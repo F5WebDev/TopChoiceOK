6b85bccbfa3ff690e851f6e85313937e5f342485
Zach Alv <learnwithzach@gmail.com>
2020-01-27 16:48:04 -0600

    updates

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -2,6 +2,7 @@ document.getElementById("navbarClick").addEventListener("click", myFunction);
 
 function myFunction() {
   document.getElementById("unorderedList").style = "opacity: 1";
+  document.getElementById("listItem9").style = "pointer-events: all";
   document.getElementById("listItem1").style = "pointer-events: all";
   document.getElementById("listItem2").style = "pointer-events: all";
   document.getElementById("listItem3").style = "pointer-events: all";
