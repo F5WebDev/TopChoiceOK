ef56bf2bfb789661688245f3c7fe99ef04a6e4cd
Zach Alv <learnwithzach@gmail.com>
2020-02-03 12:55:47 -0600

    updates

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,14 +1,6 @@
-document.getElementById("navbarClick").addEventListener("click", myFunction);
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-function myFunction() {
-  document.getElementById("unorderedList").style = "opacity: 1";
-  document.getElementById("listItem9").style = "pointer-events: all";
-  document.getElementById("listItem1").style = "pointer-events: all";
-  document.getElementById("listItem2").style = "pointer-events: all";
-  document.getElementById("listItem3").style = "pointer-events: all";
-  document.getElementById("listItem4").style = "pointer-events: all";
-  document.getElementById("listItem5").style = "pointer-events: all";
-  document.getElementById("listItem6").style = "pointer-events: all";
-  document.getElementById("listItem7").style = "pointer-events: all";
-  document.getElementById("listItem8").style = "pointer-events: all";
-}
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
